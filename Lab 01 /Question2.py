debcfc8d5de71af5cd7d431d96a1caa7cb45c338
Muhammad Usaid Khan <muhammedusaid.khan@gmail.com>
2026-01-24 16:04:24 +0500

    Create Question2.py

---
+++ Lab 01 /Question2.py
@@ -0,0 +1,11 @@
+number = int(input("Enter a number: "))
+
+count = 0
+even = 2
+print("Even Numbers: ")
+while (even <= number):
+    print(even)
+    even = even + 2
+    count = count + 1
+    
+print("Total Even Numbers: ", count)
